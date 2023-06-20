7af937ce9f0f566f0a11153fb95192ecc9b416b7
Ivan Kovalenko <gunonu@yandex.ru>
2023-06-20 16:37:36 +0300

    13 finish 1.3

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,8 @@ const cardRouter = require('./cards');
 
 router.use('/users', userRouter);
 router.use('/cards', cardRouter);
+router.use((req, res) => {
+  res.status(404).send({ message: 'Такая страница не существует' });
+});
 
 module.exports = router;
